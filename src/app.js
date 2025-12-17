68070579ac632c16efe95c0c5e527707011dea4d
Daniel luiz <danielluiz691@gmail.com>
2025-12-17 19:28:31 -0300

    att app.js

--- src/app.js
+++ src/app.js
@@ -1,29 +1,17 @@
 const express = require("express");
-const path = require("path");
 const fs = require("fs");
-const botsRoutes = require("./routes/bots");
 
 const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Rotas
-app.use("/bots", botsRoutes);
-
-// Rota teste
-app.get("/", (req, res) => {
-  res.send("Bot Panel rodando 🚀");
-});
-
 
 if (!fs.existsSync("/data")) {
   fs.mkdirSync("/data", { recursive: true });
 }
 
+app.get("/", (req, res) => {
+  res.send("Bot Panel rodando 🚀");
+});
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
-  console.log(`Painel rodando na porta ${PORT}`);
+  console.log("Painel rodando na porta", PORT);
 });
