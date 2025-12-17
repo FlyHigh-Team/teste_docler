26b32fee84895e90bddea5ef2ef02446bd20fb36
Daniel luiz <danielluiz691@gmail.com>
2025-12-17 19:48:00 -0300

    sadasds

--- src/app.js
+++ src/app.js
@@ -7,13 +7,15 @@ if (!fs.existsSync("/data")) {
   fs.mkdirSync("/data", { recursive: true });
 }
 
+app.use(express.json());
+app.use(express.static("public"));
+app.use("/bots", require("./routes/bots"));
+
 app.get("/", (req, res) => {
   res.send("Bot Panel rodando 🚀");
 });
 
-app.use(express.json());
-app.use(express.static("public"));
-app.use("/bots", require("./routes/bots"));
+
 
 
 const PORT = process.env.PORT || 3000;
